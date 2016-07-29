00412b62d17677cfe4950e19da8551af942d692d
Charles Mermelstein <charles11205@gmail.com>
2016-07-29 20:00:23 +0000

    Added user id to plan

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728182731) do
+ActiveRecord::Schema.define(version: 20160729194938) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20160728182731) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
-    t.integer  "plan_id"
     t.datetime "updated_at"
     t.string   "stripe_customer_token"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
